c67338256ca757d4e16b9f1e2fa1d99cc39ead0c
bob <bob@9r33n.net>
2011-12-20 12:16:54 -0500

    Simplify heuristic function

--- tdopt.py
+++ tdopt.py
@@ -1,20 +1,7 @@
 import sys
 import heapq
 from random import sample, random
-
-
-class Task:
-    def __init__(self, idx):
-        self.idx = idx
-
-    def __repr__(self):
-        return str(self.idx)
-
-    def affinity(self, resource, mapping):
-        return abs(self.idx - resource) / 100.0
-
-    def allows(self, resource, mapping):
-        return True
+from collections import deque
 
 
 class State:
@@ -27,15 +14,16 @@ class State:
         State.weights = {}
         for t in tasks:
             for r in resources:
-                State.weights[t, r] = t.affinity(r, {})
-                #State.weights[t, r] = random()
+                State.weights[t, r] = 0.0
 
-    def __init__(self, mapping={}, pair=None, parent=None):
+    def __init__(self, mapping={}, pair=None, parent=None, scorer=None):
+        if not scorer:
+            scorer = parent.scorer
+        self.scorer = scorer
         self.mapping = mapping
         self.pair = pair
         self.parent = parent
-        self.g = 0
-        self.f = self.h = self.heuristic()
+        self.cost = 0
 
     def __hash__(self):
         return reduce(lambda x, y : x * 17 + hash(y), 
@@ -45,82 +33,46 @@ class State:
         return self.mapping == other.mapping
 
     def __repr__(self):
-        print self.mapping
+        return str(self.mapping)
 
-    def select(self, task):
+    def _select(self, task):
         resources = State.resources - set(self.mapping.values())
-        tot = sum(State.weights[task, r] for r in resources)
-
         best = (999, 0)
         for w, r in ((State.weights[task, r], r) for r in resources):
-            if w < best[0] and random() < 0.99:
-                best = (w, r)
+            aff = self.scorer(task, r, self.mapping)
+            if w + aff < best[0]:
+                best = (w + aff, r)
         return best[1]
 
-        #while resources:
-        #rtot = 0 
-        #rw = random()
-        #for r in resources:
-        #    weight = max(0, 1.0 - State.weights[task, r])
-        #    sliver = (rtot + weight + 1) / (tot + len(resources))
-        #    rtot += sliver
-        #    if rw < rtot:
-        #        resources.remove(r)
-        #        return r
-
     def child(self):
-        # choose a task from the unmapped set
-        # this could probably be cached per search
-        tasks = [t for t in State.tasks if t not in self.mapping]
+        tasks = State.tasks - set(self.mapping.keys())
         if tasks:
-            #task = sample(tasks, 1)[0]
-            task = tasks[0]
+            task = sample(tasks, 1)[0]  # random selection for the task
+            resource = self._select(task)  # greedy selection given the task
+            m = dict(self.mapping)
+            m[task] = resource
+            return State(m, (task, resource), self)
 
-            # probalistically choose a mapping for that task based on:
-            # task requirements, task preferences, previous rewards
-            resource = self.select(task)
-            if resource >= 0:
-                if task.allows(resource, self.mapping):
-                    m = dict(self.mapping)
-                    m[task] = resource
-                    return State(m, (task, resource), self)
-
-        # we've reached a leaf node - update weights
-        tot = sum(t.affinity(r, self.mapping) for t, r in self.mapping.items())
+        # leaf node - update weights with q-learning update rule
+        score = self.scorer
+        tot = sum(score(t, r, self.mapping) for t, r in self.mapping.items())
         node = self.parent
         last = tot
         while node and node.pair:
-            w0 = State.weights[node.pair]
-            
-            # update rules (q-learning!)
-            State.weights[node.pair] += State.alpha * (State.gamma * (last - w0))
-            #cost = self.pair[0].affinity(node.pair[1], self.mapping)
-            #State.weights[node.pair] -= State.alpha * (cost + State.gamma * (last - w0))
-            #State.weights[node.pair] -= 0.25 * ((1 - cost) - (last - w0))
-            #State.weights[node.pair] += 0.1 * (cost - w0)
-            #State.weights[node.pair] = 1 - cost
-
-            last = w0
+            weight = State.weights[node.pair]
+            nudge = (State.alpha * (State.gamma * (last - weight)))
+            State.weights[node.pair] += nudge
+            last = weight
             node = node.parent
 
     def path_cost(self):
-        parent_cost = self.parent.g if self.parent else 0        
-        cost = self.pair[0].affinity(self.pair[1], self.mapping)
-        return parent_cost + cost
-
-    def heuristic(self):
-        return len(State.tasks) - len(self.mapping)
-
-    def end(self):
-        return len(self.mapping) == len(State.tasks)
+        cost = 0
+        if self.parent:
+            cost += self.parent.cost
+            cost += self.scorer(self.pair[0], self.pair[1], self.mapping)
+        return cost
 
 
-def update(node):
-    node.g = node.path_cost()
-    node.h = node.heuristic()
-    node.f = node.g + node.h
-
-                    
 def path(end):
     state = end
     while state:
@@ -129,34 +81,32 @@ def path(end):
 
 
 def search(start):
-    print 'searching'
-    frontier = []
-    heapq.heapify(frontier)
-    visited = set()
-    heapq.heappush(frontier, (start.f, start))
-    while frontier:
-        h, state = heapq.heappop(frontier)
-        visited.add(state)
-
-        c = state.child()
-        if not c:
-            return state
-
-        update(c)
-        heapq.heappush(frontier, (c.f, c))
+    node = start
+    while node:
+        node.cost = node.path_cost()
+        last = node
+        node = node.child()
+    return last
 
 
 def main():
-    tasks = [Task(i) for i in xrange(100)]
-    resources = set(range(100))
-
+    # fit 100 tasks to 100 resources - just ints here - could have properties
+    tasks = set(i for i in xrange(100))
+    resources = set(i for i in xrange(100))
     State.init(tasks, resources)
 
+    # scorers return 0 for satisfaction through 1 for extreme dissatisfaction
+    scorer0 = lambda t, r, m: abs(t - r)
+    scorer1 = lambda t, r, m: abs(t - r) / 100.0 if t % 2 == 0 else 0.0
+    scorer2 = lambda t, r, m: abs(t - r) / 100.0 if t % 5 == 0 else 0.0
+
     cost = 100
+    # iterate until we find a perfect score - might want better stop condition
     while cost > 0:
-        state = search(State())
-        print state.mapping, len(state.mapping), state.g
-        cost = state.g
+        start = State(scorer=scorer0)
+        state = search(start)
+        cost = state.path_cost()
+        print state.mapping, cost
 
 
 if __name__ == '__main__':
