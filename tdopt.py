22bcf706fc061ae1b5b14a627163ebb2a17e3748
bob <bob@9r33n.net>
2011-12-22 10:10:36 -0500

    Make tdopt purr

--- tdopt.py
+++ tdopt.py
@@ -1,7 +1,6 @@
-#!/usr/bin/env python2
-
 import sys
 from random import sample
+
 
 class State:
     @staticmethod
@@ -19,17 +18,6 @@ class State:
         self.parent = parent
         self.cost = 0
 
-    def child(self, scorer):
-        tasks = State.tasks.difference(self.mapping.keys())
-        resources = State.resources.difference(self.mapping.values())
-        if tasks and resources:
-            r = sample(resources, 1)[0]
-            task = max((State.weights[(t, r)], t) for t in tasks)[1]
-            resource = min((State.weights[(task, r)], r) for r in resources)[1]
-            m = dict(self.mapping)
-            m[task] = resource
-            return State(m, (task, resource), self)
-
     def update(self, cost):
         node = self.parent
         last = cost
@@ -40,20 +28,36 @@ class State:
             last = weight
             node = node.parent
 
-    def path_cost(self, scorer):
-        cost = 0
-        if self.parent:
-            cost += self.parent.cost
-            cost += scorer(self.pair[0], self.pair[1], self.mapping)
-        return cost
+
+# --- tdopt search functions ---
 
 
-def walk(start, scorer):
+def path_cost(node, scorer):
+    cost = 0
+    if node.parent:
+        cost += (node.parent.cost +
+                 scorer(node.pair[0], node.pair[1], node.mapping))
+    return cost
+
+
+def select_child(node, scorer):
+    tasks = State.tasks.difference(node.mapping.keys())
+    resources = State.resources.difference(node.mapping.values())
+    if tasks and resources:
+        r = sample(resources, 1)[0]
+        task = max((State.weights[t, r], t) for t in tasks)[1]
+        resource = min((State.weights[task, r], r) for r in resources)[1]
+        m = dict(node.mapping)
+        m[task] = resource
+        return State(m, (task, resource), node)
+
+
+def explore_path(start, scorer):
     node = start
     while node:
-        node.cost = node.path_cost(scorer)
+        node.cost = path_cost(node, scorer)
         last = node
-        node = node.child(scorer)
+        node = select_child(node, scorer)
 
     m = last.mapping
     cost = sum(scorer(t, r, m) for t, r in m.iteritems())
@@ -63,27 +67,42 @@ def walk(start, scorer):
 
 def search(start, scorer, end):
     i = 0
-    score, path = sys.maxint, None
-    while not end(score, i):
-        score, path = walk(start, scorer)
-        print path.mapping, score
-        yield score, path
-        i += 1
+    score = sys.maxint
+    state = None
+    last = score, state, 0
+    try:
+        while not end(score, i):
+            score, state = explore_path(start, scorer)
+            print state.mapping, score
+            yield score, state, i
+            last = score, state, i
+            i += 1
+    except KeyboardInterrupt:
+        yield last
 
+
+# --- optimization specific definitions ---
+
+
+def scorer(task, resource, mapping):
+    cost = 0
+    if resource % 10 == 0 and resource == task:
+        cost += 1000000
+    else:
+        cost += abs(resource - task) / float(len(State.tasks))
+    return cost
+        
 
 def main():
     # fit 100 tasks to 100 resources - just ints here - could have properties
     tasks = set(i for i in xrange(100))
     resources = set(i for i in xrange(100))
-    # scorers return 0 for satisfaction through 1 for extreme dissatisfaction
-    scorer = lambda t, r, m: abs(t - r) / 100.0 if t % 3 == 0 else 0.0
-
-    State.init(tasks, resources, scorer)
+    State.init(tasks, resources, scorer, alpha=0.001)
 
     # end condition can trigger based on total score or iterations
-    end = lambda x, y: x == 0 or y >= 5
-    cost, path = min(search(State(), scorer, end))
-    print path.mapping, cost
+    end = lambda score, iterations: score < 0.8
+    cost, state, i = min(search(State(), scorer, end))
+    print("best: %s cost: %s iteration: %s" % (state.mapping, cost, i))
 
 
 if __name__ == '__main__':
